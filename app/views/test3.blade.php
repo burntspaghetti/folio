b406cbe1f7d336223e1f967aaa08f2b240965ee8
singularitarian <nsbertanzetti@crimson.ua.edu>
2015-04-02 20:10:42 -0500

    nit

---
+++ app/views/test3.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SpaceshipEarth</title>
+
+    <link rel="shortcut icon" href="http://png.findicons.com/files/icons/701/devcom_network/128/globe_vista.png">
+    <!-- Bootstrap Core CSS -->
+    <link href="startbootstrap-grayscale-1.0.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="startbootstrap-grayscale-1.0.3/css/grayscale.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">
+                <i class="fa fa-play-circle"></i>  <span class="light">Nick</span> Bertanzetti
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#home">Home</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#about">Info</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#education">Education</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Contact</a>
+                </li>
+                {{--<li>--}}
+                    {{--<a class="page-scroll" href="#download">Download</a>--}}
+                {{--</li>--}}
+
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- Intro Header -->
+<header class="intro" style="background: url(images/earth.jpg)">
+    <section id="home" class="container content-section text-center">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h1>Better, Faster, Cheaper</h1>
+
+                    <p class="intro-text">Web Development</p>
+                    <a href="#about" class="btn btn-circle page-scroll">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+</header>
+
+<!-- About Section -->
+<section id="about" class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h2>Info</h2>
+            <p>2+ years experience creating value through software development and technical consulting services for clients such as:</p>
+            <p><a href="http://www.protective.com/">Protective Life Insurance</a></p>
+            <p><a href="http://www.randallreilly.com/">Randall-Reilly</a></p>
+            <p><a href="https://www.ua.edu/">The University of Alabama</a></p>
+            <p>Open to freelance and pro bono opportunities.</p>
+        </div>
+    </div>
+</section>
+
+<section id="education" class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h2>Education</h2>
+            <p>B.S. from one of the <a href="http://uanews.ua.edu/2013/07/ua-mis-program-ranked-fourth-among-public-institutions/">top ranked MIS programs</a></p>
+            <p>Coursework:</p>
+            <p>
+                Business Analysis & Project Management: Team-based intro to SDLC
+                <br>
+                Business Programming: [C++] + [Visual Studios 2010]
+                <br>
+                Business Programming II: [C#] + [Visual Studios 2010]
+                <br>
+                Application Architecture and Design: <a>[</a>C#<a>]</a> <a>+</a> <a>[</a>ASP.NET MVC4<a>]</a> <a>+</a> <a>[</a>Visual Studios 2012<a>]</a>
+                <br>
+                Database Administration: <a>[</a>SQL<a>]</a> <a>+</a> <a>[</a>Microsoft SQL Server 2012<a>]</a>
+                <br>
+                Data Communications in Global Environment: Networking, security, and internet infrastructure
+                <br>
+
+            </p>
+            <p>
+                (Tambi&eacute;n tengo un t&iacute;tulo en espa&ntilde;ol)
+            </p>
+        </div>
+    </div>
+</section>
+
+<!-- Contact Section -->
+<section id="contact" class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h2>Contact</h2>
+            <p>What can we do together?</p>
+            <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+            </p>
+            <ul class="list-inline banner-social-buttons">
+                <li>
+                    <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name"></span></a>
+                </li>
+                <li>
+                    <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name"></span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+<!-- Download Section -->
+<section id="download" class="content-section text-center">
+    <div class="download-section">
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Lorem Ipsum</h2>
+                <p></p>
+                {{--<a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>--}}
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Map Section -->
+
+
+<!-- Footer -->
+<footer>
+    <div class="container text-center">
+        <p>Copyright &copy; Your Website 2014</p>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="startbootstrap-grayscale-1.0.3/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="startbootstrap-grayscale-1.0.3/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="startbootstrap-grayscale-1.0.3/js/jquery.easing.min.js"></script>
+
+<!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="startbootstrap-grayscale-1.0.3/js/grayscale.js"></script>
+
+</body>
+
+</html>
